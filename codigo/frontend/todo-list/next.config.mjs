a61dc30a614338ba7ed44d09ae07b74dae667fa3
Rafael Radieddine <rafaelradieddine@gmail.com>
2024-06-21 01:17:19 -0300

    revert next.config

--- codigo/frontend/todo-list/next.config.mjs
+++ codigo/frontend/todo-list/next.config.mjs
@@ -1,15 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-    async redirects() {
-      return [
-        {
-          source: '/login',
-          destination: '/todo',
-          permanent: true,  // Define se o redirecionamento é permanente (HTTP 301) ou temporário (HTTP 302)
-        },
-      ];
-    },
-  };
-  
-  export default nextConfig;
-  
+const nextConfig = {};
+
+export default nextConfig;
